af02381b5e11f39af62cf30c7a73b4c7a9a025ce
Vicente Gutiérrez <gtz.rvera@gmail.com>
2022-10-17 18:43:57 -0500

    Add tests

---
+++ tests/index.js
@@ -0,0 +1,23 @@
+const helpers = require('yeoman-test');
+const assert = require('yeoman-assert');
+const path = require('path');
+
+describe('Testing project generation', () => {
+  it('generate a full node project', (done) => {
+    helpers.run(path.join(__dirname, '../generators/app'))
+      .withPrompts({
+        projectName: 'myproject',
+        projectDescription: 'My JS project description',
+        gitInit: false,
+      })
+      .then(() => {
+        assert.file('myproject/index.ts');
+        assert.file('myproject/src/controllers/HomeController.ts');
+        assert.file('myproject/src/routes/HomeRouter.ts');
+
+        done();
+      })
+      .catch(e => done(e));;
+    }
+  );
+});
